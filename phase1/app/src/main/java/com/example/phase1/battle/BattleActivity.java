1cb3bee4ecce7115801f07228ecabce763100d35
Wenxuan Sun <wenxuan.sun@mail.utoronto.ca>
2019-10-24 01:12:06 -0400

    Add BattleActivity, which represents the third game.

---
+++ phase1/app/src/main/java/com/example/phase1/battle/BattleActivity.java
@@ -0,0 +1,16 @@
+package com.example.phase1.battle;
+
+import android.app.Activity;
+// import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.phase1.R;
+
+public class BattleActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle);
+    }
+}
