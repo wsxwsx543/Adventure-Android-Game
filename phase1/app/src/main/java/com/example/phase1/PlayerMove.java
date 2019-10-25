32df841742b7e9ca57f81b100df4c98fede5ccb0
takeda <takedakaarii@gmail.com>
2019-10-25 16:01:05 -0400

    New changes which makes the project can run

--- phase1/app/src/main/java/com/example/phase1/PlayerMove.java
+++ phase1/app/src/main/java/com/example/phase1/PlayerMove.java
@@ -1,5 +1,20 @@
 package com.example.phase1;
 
-public class PlayerMove {
-    Player player;
+interface Move {
+    void fight();
 }
+
+class PlayerMove implements Move {
+    String MoveName;
+    Property property;
+
+    public PlayerMove(String MoveName, Property property){
+
+    }
+    @Override
+    public void fight() {
+        System.out.println("This is the Rectangle draw() method.");
+    }
+}
+
+
