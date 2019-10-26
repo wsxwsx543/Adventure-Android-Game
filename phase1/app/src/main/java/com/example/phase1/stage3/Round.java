fc80b13a4234b3875abddbda7eb360bc8b34bfea
Wenxuan Sun <wenxuan.sun@mail.utoronto.ca>
2019-10-26 17:24:58 -0400

    change a little bit round file.

--- phase1/app/src/main/java/com/example/phase1/stage3/Round.java
+++ phase1/app/src/main/java/com/example/phase1/stage3/Round.java
@@ -5,22 +5,21 @@ import com.example.phase1.*;
 public class Round {
     private Player player;
     private Monster monster;
-    int round_number = 0;
     private MoveFactory moveFactory;
+    // put round number in battle activity
 
     public Round(Player player, Monster monster){
         this.player = player;
         this.monster = monster;
-        this.round_number += 1;
     }
 
     public void battle(){
         Property playermove = moveFactory.playerDoMove("a", player);
         Property monstermove = moveFactory.monsterDoMove("a", monster);
+        Property player_property = player.getProperty(); //copy
+        // player.getProperty().addPropertyToSelf(playermove);
+        // monster.getProperty().addPropertyToSelf(monstermove);
     }
 
-    int getRound_number(){
-      return this.round_number;
-    }
 
 }
