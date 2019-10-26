12e007324aef48b0e16b1904c3b5cbfacd8802c2
Wenxuan Sun <wenxuan.sun@mail.utoronto.ca>
2019-10-25 23:01:52 -0400

    battle for round file in stage 3.

--- phase1/app/src/main/java/com/example/phase1/stage3/Round.java
+++ phase1/app/src/main/java/com/example/phase1/stage3/Round.java
@@ -1,16 +1,22 @@
 package com.example.phase1.stage3;
 
-import com.example.phase1.Player;
+import com.example.phase1.*;
 
 public class Round {
     private Player player;
     private Monster monster;
     int round_number = 0;
+    private MoveFactory moveFactory;
 
     public Round(Player player, Monster monster){
         this.player = player;
         this.monster = monster;
         this.round_number += 1;
+    }
+
+    public void battle(){
+        Property playermove = moveFactory.playerDoMove("a", player);
+        Property monstermove = moveFactory.monsterDoMove("a", monster);
     }
 
     int getRound_number(){
