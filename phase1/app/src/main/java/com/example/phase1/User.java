3be9684b245481e39d3e55dc8dcb411479baa166
wusixuan <wusixuan543543@gmail.com>
2019-10-27 17:54:57 -0400

    Make user can create a new player and allow user to select the initial career for player.

--- phase1/app/src/main/java/com/example/phase1/User.java
+++ phase1/app/src/main/java/com/example/phase1/User.java
@@ -5,33 +5,35 @@ import java.util.HashMap;
 
 public class User implements Serializable {
 
-
     // username of the User
     private String username;
     // password of the User
     private String password;
-    // The limit of how many players this User can initialize
-//    private int playerNumLimit;
-//    // List of all players the User initialized;
-    private PlayerManager playerManager;
+    // Current player for this user.
+    private Player curPlayer;
+    // max number of players for a user.
+    private int playersNumberLimit;
+    // Players this user have
+    private HashMap<String, Player> players;
 
-    // Key of this hash map is player's name and value is the player.
-    private HashMap<String, Player> playerHashMap;
-
-    public User(String username, String password){
-        playerManager = new PlayerManager();
+    public User(String username, String password, int playersNumberLimit){
         setUsername(username);
         setPassword(password);
+        setPlayersNumberLimit(playersNumberLimit);
+        players = new HashMap<>();
+        curPlayer = null;
     }
 
-    public void addPlayer(Player player){
-        playerManager.addPlayer(player);
+    public void setPlayersNumberLimit(int playersNumberLimit) {
+        this.playersNumberLimit = playersNumberLimit;
+    }
+    public int getPlayersNumberLimit() {
+        return playersNumberLimit;
     }
 
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
@@ -39,8 +41,64 @@ public class User implements Serializable {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setCurPlayer(Player curPlayer){
+        this.curPlayer = curPlayer;
+    }
+    public Player getCurPlayer(){
+        return this.curPlayer;
+    }
+
+    public HashMap<String, Player> getPlayers() {
+        return players;
+    }
+    public void setPlayers(HashMap<String, Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) throws TooMuchPlayersException, SamePlayerNameException, EmptyPlayerNameException{
+        if(players.size() >= playersNumberLimit) {
+            throw new TooMuchPlayersException();
+        }
+        else if(players.containsKey(player.getName())){
+            throw new SamePlayerNameException();
+        }
+        else if(player.getName().equals("")){
+            throw new EmptyPlayerNameException();
+        }
+        else players.put(player.getName(), player);
+    }
+
+    // Return the player creates highest attack.
+    public Player findHighestAttackCreate(){
+        int highest = 0;
+        Player ret_player = new Player("NoPlayer", new Property(0, 0, 0,
+                0));
+        for(Player player: players.values()){
+            if(player.getAttackCreate() > highest){
+                highest = player.getAttackCreate();
+                ret_player = player;
+            }
+        }
+        return ret_player;
+    }
+}
+
+class TooMuchPlayersException extends Exception{
+    public TooMuchPlayersException(){
+        super();
+    }
+}
+class SamePlayerNameException extends Exception{
+    public SamePlayerNameException(){
+        super();
+    }
+}
+class EmptyPlayerNameException extends Exception{
+    EmptyPlayerNameException(){
+        super();
     }
 }
