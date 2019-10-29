263a5dd3983271876c69f36978a2c487849c499d
Samuel <samuelyixp@outlook.com>
2019-10-29 15:59:49 -0400

    Added classes and functionalities TreasureHunt is now semi runnable

--- phase1/app/src/main/java/com/example/phase1/stage2/Treasure.java
+++ phase1/app/src/main/java/com/example/phase1/stage2/Treasure.java
@@ -2,8 +2,13 @@ package com.example.phase1.stage2;
 
 import com.example.phase1.R;
 
-public class Treasure extends Unit {
-    public Treasure(){
-        imageID = R.drawable.baoxiang2;
+public class Treasure extends Box {
+    public Treasure(int x, int y){
+        super(x, y);
+    }
+    public void update(){
+        if (expanded){
+            imageID = R.drawable.baoxiang2;
+        }
     }
 }
--- phase1/app/src/main/java/com/example/phase1/stage2/Unit.java
+++
@@ -1,7 +0,0 @@
-package com.example.phase1.stage2;
-
-import java.util.ArrayList;
-
-public abstract class Unit {
-    public int imageID;
-}
