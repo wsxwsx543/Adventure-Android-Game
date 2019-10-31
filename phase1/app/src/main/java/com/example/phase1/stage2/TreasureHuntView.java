0d5d0df4bc3ae061220c906587636a18134d4735
Samuel <samuelyixp@outlook.com>
2019-10-31 16:59:48 -0400

    Added property display and feature that saves file after transition for stage 2

--- phase1/app/src/main/java/com/example/phase1/stage2/TreasureHuntView.java
+++ phase1/app/src/main/java/com/example/phase1/stage2/TreasureHuntView.java
@@ -3,22 +3,36 @@ package com.example.phase1.stage2;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import com.example.phase1.stage1.g1moveActivity;
+import com.example.phase1.FileSystem;
+import com.example.phase1.Player;
+import com.example.phase1.Property;
+import com.example.phase1.User;
+import com.example.phase1.UserManager;
 import com.example.phase1.stage3.BattleActivity;
 
 import java.util.ArrayList;
 
 public class TreasureHuntView extends SurfaceView implements Runnable {
 
+    //Paint for text
+    Paint textPaint;
 
-    public float curX = -1, curY = -1;
+    //Cursor location
+    public double curX = -1, curY = -1;
+
+    //Current user
+    private User user = UserManager.getInstance().getCurUser();
+    private Property property = user.getCurPlayer().getProperty();
+    private Player player = user.getCurPlayer();
+
     public Box[][] boxes;
     private BoxesMapper boxesMapper;
     private ArrayList<Bitmap> boxesBitmap;
@@ -27,15 +41,21 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
     public Thread thread;
     public SurfaceHolder holder;
     public boolean running;
-
+    private FileSystem fileSystem;
 
     //Temp var
-
-    private boolean switcher = false;
 
     public TreasureHuntView(Context context, int boardWidth, int boardLength, int unit_size, int startX, int startY) {
         super(context);
 
+        this.textPaint = new Paint();
+
+        textPaint.setColor(Color.rgb(255, 215, 0));
+        textPaint.setTextSize(70);
+        textPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        textPaint.setAntiAlias(true);
+
+        this.fileSystem = new FileSystem(context);
         this.holder = getHolder();
         this.boardWidth = boardWidth;
         this.boardLength = boardLength;
@@ -48,7 +68,7 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
     }
 
     public TreasureHuntView(Context context) {
-        this(context, 10, 15, 108, 0, 200);
+        this(context, 15, 18, 72, 0, 300);
     }
 
     public void pause() {
@@ -73,22 +93,24 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
 
             if (curX != -1 && curY != -1) {
                 int[] pair = getCurBoxLoc(curX, curY);
-                if (register(pair)){
+                if (register(pair)) {
                     boxes[pair[1]][pair[0]].expand(new ArrayList<>());
                 }
+                curX = -1;
+                curY = -1;
             }
 
             //update boxes
             update();
             //Draw the boxes
             draw();
-
+            //Check if trap is triggered
             checkEnded();
 
         }
     }
 
-    private int[] getCurBoxLoc(float curX, float curY) {
+    private int[] getCurBoxLoc(double curX, double curY) {
         int[] pair = new int[2];
         int x = (int) ((curX - this.startX) / unit_size);
         int y = (int) ((curY - this.startY) / unit_size);
@@ -97,8 +119,8 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
         return pair;
     }
 
-    private boolean register(int[] pair){
-        if (pair[0] >= 0 && pair[0] <= boardWidth - 1&& pair[1] >= 0 && pair[1] <= boardLength - 1){
+    private boolean register(int[] pair) {
+        if (pair[0] >= 0 && pair[0] <= boardWidth - 1 && pair[1] >= 0 && pair[1] <= boardLength - 1) {
             return true;
         }
         return false;
@@ -120,12 +142,13 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
             }
         }
     }
-    private void checkEnded(){
+
+    private void checkEnded() {
         for (int y = 0; y < this.boardLength; y++) {
             for (int x = 0; x < this.boardWidth; x++) {
-                if (boxes[y][x] instanceof Trap && boxes[y][x].expanded){
+                if (boxes[y][x] instanceof Trap && boxes[y][x].expanded) {
                     try {
-                        thread.sleep(5000);
+                        thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -136,11 +159,20 @@ public class TreasureHuntView extends SurfaceView implements Runnable {
         }
     }
 
-    private void draw(){
+    public void saveUser() {
+        fileSystem.save(user, "Users.ser");
+    }
+
+    private void draw() {
         if (holder.getSurface().isValid()) {
             Canvas canvas = getHolder().lockCanvas();
             // Draw background
             canvas.drawARGB(255, 255, 255, 255);
+            // Draw the stats of the player
+            canvas.drawText("Attack: " + property.getAttack(), 20, (float) (startY + boardLength * unit_size + 100), textPaint);
+            canvas.drawText("Defence: " + property.getDefence(), 500, (float) (startY + boardLength * unit_size + 100), textPaint);
+            canvas.drawText("Flexibility: " + property.getFlexibility(), 20, (float) (startY + boardLength * unit_size + 200), textPaint);
+            canvas.drawText("Luckiness: " + property.getLuckiness(), 500, (float) (startY + boardLength * unit_size + 200), textPaint);
 
             for (int y = 0; y < this.boardLength; y++) {
                 for (int x = 0; x < this.boardWidth; x++) {
