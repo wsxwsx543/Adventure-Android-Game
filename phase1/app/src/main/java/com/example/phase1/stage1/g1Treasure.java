13e4bb07de4499b7953ca3feed41db832bf4ee68
Jessica <jiangyue.yu@mail.utoronto.ca>
2019-10-31 23:16:49 -0400

    delete unnecessary methods.

--- phase1/app/src/main/java/com/example/phase1/stage1/g1Treasure.java
+++ phase1/app/src/main/java/com/example/phase1/stage1/g1Treasure.java
@@ -13,13 +13,16 @@ public class g1Treasure {
     /**
      * The x, y coordinate and the length and width of the treasure image
      */
-    int x = 1008, y = 720, width = 72, height = 72;
-    Bitmap treasurerview;
+    int x, y, width = 72, height = 72;
+    private Bitmap treasurerview;
 
     /**
      * @param res
      */
-    g1Treasure(Resources res){
+    g1Treasure(int curr_x, int curr_y, Resources res){
+        this.x = curr_x;
+        this.y = curr_y;
+
         treasurerview = BitmapFactory.decodeResource(res, R.drawable.baoxiang2);
 
         treasurerview = Bitmap.createScaledBitmap(treasurerview, width, height, false);
