fcc0487e2b7cfa40d2c4791fac497fd1d61ead6f
wusixuan <wusixuan543543@gmail.com>
2019-11-15 19:52:50 -0500

    Add comments to initializable interface.

--- phase2/app/src/main/java/com/example/phase1/Initializable.java
+++ phase2/app/src/main/java/com/example/phase1/Initializable.java
@@ -1,5 +1,7 @@
 package com.example.phase1;
 
+/** This interface is implemented when an Activity class need to be initialized.**/
+
 public interface Initializable {
     /** Initializable. */
     public void init();
