dc51d925b7ca3bb02d6e6ceb5bc98a952cf03802
Jessica <jiangyue.yu@mail.utoronto.ca>
2019-11-30 19:40:10 -0500

    change MazeObjectsFactory to package private to avoid warning.

--- phase2/app/src/main/java/com/example/phase2/stage1/model/MazeObjectsFactory.java
+++ phase2/app/src/main/java/com/example/phase2/stage1/model/MazeObjectsFactory.java
@@ -6,8 +6,8 @@ import android.content.res.Resources;
  * Factory to return different class instance according to different inputs
  * can return treasure, monster, door
  */
-public class MazeObjectsFactory {
-    public static MazeObjects getMazeObject(String what, int curr_x, int curr_y, Resources res, String type){
+class MazeObjectsFactory {
+     static MazeObjects getMazeObject(String what, int curr_x, int curr_y, Resources res, String type){
         if("Treasure".equalsIgnoreCase(what)) return new Treasure(curr_x, curr_y, res, type);
         else if("Monster".equalsIgnoreCase(what)) return new Monster(curr_x, curr_y, res, type);
         else if("Door".equalsIgnoreCase(what)) return new Door(curr_x, curr_y, res, type);
